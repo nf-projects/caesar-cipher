0c9fefbbdd87edc74e998cc85ba1c7c7e0876719
Spectre <nf-projects@users.noreply.github.com>
2022-12-09 10:59:12 -0800

    remove useless comments

--- util/hacker.ts
+++ util/hacker.ts
@@ -11,7 +11,6 @@ async function fetchData() {
 }
 
 function isEnglishWord(word: string): boolean {
-  // if input is in wordList, return true
   for (let i = 0; i < wordList.length; i++) {
     if (wordList[i] === word) {
       return true;
@@ -21,22 +20,14 @@ function isEnglishWord(word: string): boolean {
 }
 
 export async function crackCaesarCipher(input: string): Promise<number> {
-  // fetchData() should be awaited so that wordList is populated
-  // before isEnglishWord() is called
   await fetchData();
 
   let maxMatches = 0;
   let maxMatchesKey = -1;
   
-//   return (await isEnglishWord(input))
     for (let i = 0; i < 26; i++) {
         const decrypted = caesarEncrypt(input, i, false);
-        // console.log("Input: " + input)
-        /////console.log("----------------------")
         const decryptedArray = decrypted.replace(/[^a-zA-Z ]/g, "").split(" ");
-        // console.log("Decrypted: " + decryptedArray + " Key: " + i);
-        // log every word in the decrypted array to console in a single line
-        /////console.log(decryptedArray.join(" ") + " Key: " + i);
         let matches = 0;
         for (let j = 0; j < decryptedArray.length; j++) {
             if (isEnglishWord(decryptedArray[j])) {
@@ -47,7 +38,6 @@ export async function crackCaesarCipher(input: string): Promise<number> {
             maxMatches = matches;
             maxMatchesKey = i;
         }
-        /////console.log(matches)
     }
     return maxMatchesKey;
 }
